05b80ad5e235e5463e93632cfb18424a5791c550
Timothy Saudners <timsaunders88@gmail.com>
2020-10-09 13:43:09 +0100

    added UI integration test for uodating display with the reult of the previous operation, and chaining multiple operations together

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -7,6 +7,30 @@ describe('calculator', () => {
 
   it('should have working number buttons', () => {
     cy.get('#number2').click();
-    cy.get('.display').should('contain', '2')
+    cy.get('.display').should('contain', '2');
   })
+
+  it('should update the display with the result of the previous operation', () => {
+    cy.get('#number8').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number2').click();
+    //should display number just clicked at this stage
+    cy.get('.display').should('contain', '2');
+    cy.get('#operator_subtract').click();
+    //should display resut of previous operation at this stage
+    cy.get('.display').should('contain', '4');
+  })
+
+  it('should be able to chain multiple operations together', () => {
+    cy.get('#number8').click();
+    cy.get('#operator_divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number3').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '1')
+  })
+
+
 })
+
