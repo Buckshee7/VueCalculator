de347fe42af4254445dd99ff4c8bf5e49cc7f240
Timothy Saudners <timsaunders88@gmail.com>
2020-10-09 12:01:23 +0100

    added unit test for subtract function

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -3,10 +3,20 @@ import { shallowMount } from '@vue/test-utils'
 import App from '@/App.vue'
 
 describe('App.vue', () => {
-  it('enterNum changes running total', () => {
+  it('add function changes running total', () => {
     const wrapper = shallowMount(App)
     wrapper.vm.previousTotal = 4
     wrapper.vm.add('5');
     expect(wrapper.vm.runningTotal).to.equal(9)
   })
+
+  it('subtract function changes running total', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 7
+    wrapper.vm.subtract('4');
+    expect(wrapper.vm.runningTotal).to.equal(3)
+  })
+
+  
+
 })
